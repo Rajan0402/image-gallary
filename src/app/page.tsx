399d7d15d1d420d7d16a5a268edff649dfcd8b94
Rajan Yadav <rajanyadav2873@gmail.com>
2024-06-22 15:50:06 +0530

    basic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockUrls=[
+  // "https://utfs.io/f/1b28723c-f1cf-4586-8822-d0846f705caf-z9awnr.jpg",
+  "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg",
+  "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg",
+  "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg",
+  "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
+console.log(mockImages)
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div className="max-w-96">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       test
     </main>
   );
