74c129ce2116d7c4effac804f7138cbd7e1457f1
Rajan Yadav <rajanyadav2873@gmail.com>
2024-06-24 16:20:41 +0530

    home page image test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 
 const mockUrls=[
-  // "https://utfs.io/f/1b28723c-f1cf-4586-8822-d0846f705caf-z9awnr.jpg",
   "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg",
   "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg",
   "https://utfs.io/f/41e9b350-2436-48b8-b1b8-279a74a5a403-tz9yt0.jpg",
@@ -13,15 +11,13 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }))
 
-console.log(mockImages)
-
 export default function HomePage() {
   return (
     <main>
       <div className="flex flex-wrap gap-4">
         {mockImages.map((image) => (
-          <div className="max-w-96">
-            <Image src={image.url} alt="test"/>
+          <div key={image.id} className="max-w-96">
+            <img src={image.url} alt="test"/>
           </div>
         ))}
       </div>
