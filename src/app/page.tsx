6a12692e3436a00fc20bd9b8ed9bf182e84ca830
Rajan Yadav <rajanyadav2873@gmail.com>
2024-06-24 20:07:54 +0530

    image upload added using uploadThing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,30 @@ import { db } from "@/server/db";
 
 export const dynamic = "force dynamic"
 
+async function Image() {
+
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  })
+
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="max-w-96">
+          <img src={image.url} alt={image.name}/>
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 export default async function HomePage() {
 
-  const images = await db.query.images.findMany()
 
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="max-w-96">
-            <img src={image.url} alt={image.name}/>
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-      test
+        <Image/>
     </main>
   );
 }
